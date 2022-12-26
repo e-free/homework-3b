6e79861929b7b1f57ca2d64f2fff58b5d5183fad
e-free <e-free@mail.ru>
2022-12-26 23:55:35 +0800

    добавление цвета фона остатков товара и отображение иконки валюты

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,24 +1,45 @@
 function Listing (props){
-/*<img src={elem['MainImage'].url_570xN} alt=""/>*/
   let item = [];
+  let levelGrade = "";
+  let costWithCurrency ="";
+  
   if (props.item) { item = props.item };
-  console.log("item: ", item);
-  console.log(item[0]['MainImage']['url_570xN']);
+
   let items = item.map(function(elem, index){
-      return <>      
-        <div id = {elem.listing_id} className ="item">
-        <div className ="item-image">
-          <a href={elem.url}>
-            <img src={(elem.MainImage && elem.MainImage.url_570xN) ? elem.MainImage.url_570xN : 'no image'} alt ={elem.title}  />
-          </a>
-        </div>
-        <div className ="item-details">
-          <p className ="item-title">{elem.title}</p>
-          <p className ="item-price">{elem.price}</p>
-          <p className ="item-quantity level-medium">{elem.quantity} left</p>
-        </div>
+   if (elem.currency_code === "USD"){
+    costWithCurrency = "$"+ String(elem.price); 
+   } 
+   else if (elem.currency_code === "EUR"){
+    costWithCurrency = "€" + String(elem.price);
+   }
+   else {
+    costWithCurrency = String(elem.price) + " " + elem.currency_code;
+   }
+
+
+  if(elem.quantity <= 20 && elem.quantity > 10){
+    levelGrade = "level-medium";
+  }
+  else if (elem.quantity <= 10){
+    levelGrade = "level-low";
+  }
+  else {
+    levelGrade = "level-high";
+  }
+  return <>      
+    <div id = {elem.listing_id} className ="item">
+      <div className ="item-image">
+        <a href={elem.url}>
+          <img src={(elem.MainImage && elem.MainImage.url_570xN) ? elem.MainImage.url_570xN : 'no image'} alt ={elem.title}  />
+        </a>
       </div>
-    </>
+      <div className ="item-details">
+        <p className ="item-title">{elem.title}</p>
+        <p className ="item-price">{costWithCurrency}</p>
+        <p className ={`item-quantity ${levelGrade}`}>{elem.quantity} left</p>
+      </div>
+    </div>
+  </>
     
   });
 
